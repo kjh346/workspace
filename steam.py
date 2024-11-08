6f8bf0c7b8ed59a26ab307029a5a2a5f83e84169
김재헌 <skiel20141209@gmail.com>
2024-11-08 11:57:38 +0900

    1234

--- steam.py
+++ steam.py
@@ -5,7 +5,7 @@ import csv
 import pandas as pd
 
 # 스팀의 특정 게임 리뷰 페이지 URL
-game_id = '570'  # 예: 도타 2 (게임 ID는 Steam store URL에서 확인 가능)
+game_id = '1245620' 
 url = f"https://store.steampowered.com/appreviews/{game_id}?json=1&filter=recent"
 
 # 요청 헤더 설정
@@ -34,12 +34,14 @@ with open('reviews.csv', mode='w', encoding='utf-8', newline='') as file:
         review_text = review['review']
         recommendation = "추천" if review['voted_up'] else "비추천"
         review_date = review['timestamp_created']
+        playtime = review['author']['playtime_forever']
         
-        writer.writerow([review_text, recommendation, review_date])
+        writer.writerow([review_text, recommendation, review_date, playtime])
 
         print("리뷰 내용:", review_text)
         print("추천 여부:", recommendation)
         print("리뷰 날짜:", review_date)
+        print("플레이 시간(분):", playtime)
         print("="*50)
 
     # 딜레이 추가 (스팀 서버에 과부하를 방지)
